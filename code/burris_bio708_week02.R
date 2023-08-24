87594f8a8bb44884a0dbcc1418e15064ff70fb47
wadeburris <wadeburris@gmail.com>
2023-08-24 10:46:05 -0400

    Week 2 Thur

--- code/burris_bio708_week02.R
+++ code/burris_bio708_week02.R
@@ -193,3 +193,130 @@ abdVA <- mean(VA$abundance)
 abdNC <- mean(NC$abundance)
 abdVA #mean abundance in VA
 abdNC #mean abundance in NC
+
+#alt shift o 
+
+
+# tidyverse ---------------------------------------------------------------
+
+
+#install.packages("tidyverse")
+library(tidyverse)
+
+
+iris <- as_tibble(iris)
+iris
+
+# filter -----------
+
+# single match "=="
+filter(iris, Species == "virginica")
+
+# multiple match "%in%"
+filter(iris, Species %in% c("virginica", "versicolor"))
+
+# except "!="
+filter(iris, Species != "virginica")
+
+# except multiple "!(x %in% c("a", "b"))
+filter(iris, !(Species %in% c("virginica", "versicolor")))
+
+# greater than ">"
+filter(iris, Sepal.Length > 5)
+
+# equal & greater than ">="
+filter(iris, Sepal.Length >= 5)
+
+# less than "<"
+filter(iris, Sepal.Length < 5)
+
+# equal & less than "<="
+filter(iris, Sepal.Length <= 5)
+
+
+# arrange -----------------------------------------------------------------
+
+# arrange in an ascending order
+arrange(iris, Sepal.Length)
+
+# arrange in an descending order
+arrange(iris, desc(Sepal.Length))
+# select one column
+select(iris, Sepal.Length)
+
+
+# select ------------------------------------------------------------------
+
+
+# select multiple columns
+select(iris, c(Sepal.Length, Sepal.Width))
+
+# remove one column
+select(iris, -Sepal.Length)
+
+# remove multiple columns
+select(iris, -c(Sepal.Length, Sepal.Width))
+
+# select/remove multiple columns with a start rule
+# starts_with("x")
+select(iris, starts_with("Sepal"))
+select(iris, -starts_with("Sepal"))
+
+# select/remove multiple columns with an end rule
+# ends_with("x")
+select(iris, ends_with("Width"))
+select(iris, -ends_with("Width"))
+
+
+# mutate ------------------------------------------------------------------
+
+# add a new column
+x <- 1:150
+mutate(iris, oowee = x)
+
+
+# piping ------------------------------------------------------------------
+
+
+# the following codes produce the same data frame
+# apply functions separately
+df_vir <- filter(iris, Species == "virginica")
+df_vir_sl <- select(df_vir, Sepal.Length)
+print(df_vir_sl)
+
+# piping (command shift m)
+iris %>% 
+  filter(Species == "virginica") %>% 
+  select(Sepal.Length)
+
+
+# group operations --------------------------------------------------------
+
+
+# grouping by "Species", then take means "Speal.Length" for each species
+iris %>% 
+  group_by(Species) %>% 
+  summarize(mu_sl = mean(Sepal.Length))
+
+# grouping by "Species", then take means & SD "Speal.Length" for each species
+iris %>% 
+  group_by(Species) %>% 
+  summarize(mu_sl = mean(Sepal.Length),
+            sd_sl = sd(Sepal.Length))
+
+# matching by a single column
+## left join by "Species": one to one
+df1 <- tibble(Species = c("A", "B", "C"),
+              x = c(1, 2, 3))
+
+df2 <- tibble(Species = c("A", "B", "C"),
+              y = c(4, 5, 6))
+
+left_join(x = df1,
+          y = df2,
+          by = "Species")
+
+
+# Tidyverse exercises 1-4 -------------------------------------------------
+
+
